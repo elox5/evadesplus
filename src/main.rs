6ecc052084b3fbd1de9b195d54cff237c3ea5c05
elOx <elox55555@gmail.com>
2024-12-05 15:32:22 +0200

    Add basic warp server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let root_route = warp::fs::dir("static");
+
+    warp::serve(root_route).run(([127, 0, 0, 1], 3333)).await;
+
+    Ok(())
 }
