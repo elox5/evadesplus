ee0a49d100f32b665fd707196d269310d19301a3
elOx <elox55555@gmail.com>
2025-01-06 19:16:54 +0200

    Extract parts of tokio::select macro into own functions
    This was necessary for formatting support as code inside the macro doesn't get formatted

--- src/networking/webtransport.rs
+++ src/networking/webtransport.rs
@@ -1,4 +1,4 @@
-use super::chat::ChatRequest;
+use super::{chat::ChatRequest, leaderboard::LeaderboardUpdatePacket};
 use crate::{
     game::game::{Game, Player},
     physics::vec2::Vec2,
@@ -12,9 +12,10 @@ use std::{
 };
 use tokio::sync::{broadcast, Mutex};
 use wtransport::{
+    datagram::Datagram,
     endpoint::{endpoint_side::Server, IncomingSession},
     error::ConnectionError,
-    Endpoint, Identity, ServerConfig,
+    Connection, Endpoint, Identity, RecvStream, SendStream, ServerConfig,
 };
 
 pub struct WebTransportServer {
@@ -110,126 +111,187 @@ impl WebTransportServer {
         loop {
             tokio::select! {
                 stream = connection.accept_uni() => {
-                    let mut stream = stream?;
-
-                    let bytes_read = match stream.read(&mut buffer).await? {
-                        Some(bytes_read) => bytes_read,
-                        None => continue,
-                    };
-
-                    let data = &buffer[..bytes_read];
-                    let header = &data[0..4];
-                    let data = &data[4..];
-
-                    match header {
-                        b"NAME" => {
-                            let name = std::str::from_utf8(data);
-
-                            if let Ok(name) = name {
-                                println!("Accepted name '{name}' from client {id}. Spawning hero...");
-
-                                let mut game = game.lock().await;
-                                let leaderboard_state = game.leaderboard_state.clone();
-
-                                let player_arcswap = game.spawn_hero(name, connection.clone()).await;
-                                player = Some(player_arcswap.clone());
-
-                                let area = player_arcswap.load().area.clone();
-
-                                let definition = area.lock().await.definition_packet();
-
-                                if !leaderboard_state.is_empty() {
-                                    let mut state_stream = connection.open_uni().await?.await?;
-                                    state_stream.write_all(&leaderboard_state.to_bytes()).await?;
-                                    state_stream.finish().await?;
-                                }
-
-                                let mut def_stream = connection.open_uni().await?.await?;
-                                def_stream.write_all(&definition).await?;
-                                def_stream.finish().await?;
-                            }
-                        },
-                        b"CHAT" => {
-                            if let Some(player) = &player {
-                                let text = std::str::from_utf8(data);
-
-                                if let Ok(text) = text {
-                                    let name = player.load().name.clone();
-
-                                    let request = ChatRequest::new(text.to_owned(), name.clone());
-
-                                    let _ = chat_tx.send(request);
-                                }
-                            }
-                        },
-                        _ => {
-                            println!("Received unknown packet from client {id} (header: {})", std::str::from_utf8(header).unwrap_or(&format!("{header:x?}").clone()));
-                        }
-                    }
+                    let stream = stream?;
+                    Self::handle_uni_stream(stream, &mut buffer, &connection, &game, id, &mut player, &chat_tx).await?;
                 }
                 streams = connection.accept_bi() => {
                     let streams = streams?;
-                    let (mut send_stream, mut recv_stream) = streams;
-
-                    let bytes_read = match recv_stream.read(&mut buffer).await? {
-                        Some(bytes_read) => bytes_read,
-                        None => continue,
-                    };
-
-                    let data = &buffer[..bytes_read];
-                    let text = std::str::from_utf8(data);
-
-                    if let Ok(text) = text {
-                        if text == "ping" {
-                            send_stream.write_all(b"pong").await?;
-                        }
-                    }
-
-                    send_stream.finish().await?;
+                    let (send_stream, recv_stream) = streams;
+                    Self::handle_bi_stream(send_stream, recv_stream, &mut buffer).await?;
                 }
                 dgram = connection.receive_datagram() => {
-                    if let Some(ref player) = player {
-                        let dgram = dgram?;
-                        let payload = dgram.payload();
-
-                        let x = f32::from_le_bytes([payload[0], payload[1], payload[2], payload[3]]);
-                        let y = f32::from_le_bytes([payload[4], payload[5], payload[6], payload[7]]);
-
-                        // println!("Received input '({x:.2}, {y:.2})' from client {id}");
-
-                        let mut game = game.lock().await;
-                        game.update_player_input(player.load().entity, Vec2::new(x, y)).await;
-                    }
+                    let dgram = dgram?;
+                    Self::handle_datagram(dgram, &game, &mut player).await;
                 }
                 connection_result = connection.closed() => {
-                    println!("Connection from client {id} closed");
-
-                    if let Some(player) = player {
-                        println!("Despawning player {:?} from client {id}", player.load().entity);
-                        let mut game = game.lock().await;
-                        println!("Game lock acquired");
-                        game.despawn_hero(player.load().entity).await;
-                    }
-
-                    return Ok(connection_result);
+                    return Self::handle_connection_closed(connection_result, &game, &player, id).await;
                 }
                 leaderboard_update = lb_rx.recv() => {
-                    if let Ok(leaderboard_update) = leaderboard_update {
-                        let mut update_stream = connection.open_uni().await?.await?;
-
-                        update_stream.write_all(&leaderboard_update.to_bytes()).await?;
-                        update_stream.finish().await?;
-                    }
+                    let leaderboard_update = leaderboard_update?;
+                    Self::handle_leaderboard_update(leaderboard_update, &connection).await?;
                 }
                 chat_broadcast = chat_rx.recv() => {
-                    if let Ok(chat_broadcast) = chat_broadcast {
-                        let mut chat_stream = connection.open_uni().await?.await?;
-
-                        chat_stream.write_all(&chat_broadcast.to_bytes()).await?;
-                        chat_stream.finish().await?;
-                    }
+                    let chat_broadcast = chat_broadcast?;
+                    Self::handle_chat_broadcast(chat_broadcast, &connection).await?;
                 }
             }
         }
+    }
+
+    async fn handle_uni_stream(
+        mut stream: RecvStream,
+        mut buffer: &mut Box<[u8]>,
+        connection: &Connection,
+        game: &Arc<Mutex<Game>>,
+        id: u64,
+        player: &mut Option<Arc<ArcSwap<Player>>>,
+        chat_tx: &broadcast::Sender<ChatRequest>,
+    ) -> Result<()> {
+        let bytes_read = match stream.read(&mut buffer).await? {
+            Some(bytes_read) => bytes_read,
+            None => return Ok(()),
+        };
+
+        let data = &buffer[..bytes_read];
+        let header = &data[0..4];
+        let data = &data[4..];
+
+        match header {
+            b"NAME" => {
+                let name = std::str::from_utf8(data);
+
+                if let Ok(name) = name {
+                    println!("Accepted name '{name}' from client {id}. Spawning hero...");
+
+                    let mut game = game.lock().await;
+                    let leaderboard_state = game.leaderboard_state.clone();
+
+                    let player_arcswap = game.spawn_hero(name, connection.clone()).await;
+                    *player = Some(player_arcswap.clone());
+
+                    let area = player_arcswap.load().area.clone();
+
+                    let definition = area.lock().await.definition_packet();
+
+                    if !leaderboard_state.is_empty() {
+                        let mut state_stream = connection.open_uni().await?.await?;
+                        state_stream
+                            .write_all(&leaderboard_state.to_bytes())
+                            .await?;
+                        state_stream.finish().await?;
+                    }
+
+                    let mut def_stream = connection.open_uni().await?.await?;
+                    def_stream.write_all(&definition).await?;
+                    def_stream.finish().await?;
+                }
+            }
+            b"CHAT" => {
+                if let Some(player) = &player {
+                    let text = std::str::from_utf8(data);
+
+                    if let Ok(text) = text {
+                        let name = player.load().name.clone();
+
+                        let request = ChatRequest::new(text.to_owned(), name.clone());
+
+                        let _ = chat_tx.send(request);
+                    }
+                }
+            }
+            _ => {
+                println!(
+                    "Received unknown packet from client {id} (header: {})",
+                    std::str::from_utf8(header).unwrap_or(&format!("{header:x?}").clone())
+                );
+            }
+        }
+
+        Ok(())
+    }
+
+    async fn handle_bi_stream(
+        mut send_stream: SendStream,
+        mut recv_stream: RecvStream,
+        mut buffer: &mut Box<[u8]>,
+    ) -> Result<()> {
+        let bytes_read = match recv_stream.read(&mut buffer).await? {
+            Some(bytes_read) => bytes_read,
+            None => return Ok(()),
+        };
+
+        let data = &buffer[..bytes_read];
+        let text = std::str::from_utf8(data);
+
+        if let Ok(text) = text {
+            if text == "ping" {
+                send_stream.write_all(b"pong").await?;
+            }
+        }
+
+        send_stream.finish().await?;
+
+        Ok(())
+    }
+
+    async fn handle_datagram(
+        datagram: Datagram,
+        game: &Arc<Mutex<Game>>,
+        player: &mut Option<Arc<ArcSwap<Player>>>,
+    ) {
+        if let Some(ref player) = player {
+            let payload = datagram.payload();
+
+            let x = f32::from_le_bytes([payload[0], payload[1], payload[2], payload[3]]);
+            let y = f32::from_le_bytes([payload[4], payload[5], payload[6], payload[7]]);
+
+            // println!("Received input '({x:.2}, {y:.2})' from client {id}");
+
+            let mut game = game.lock().await;
+            game.update_player_input(player.load().entity, Vec2::new(x, y))
+                .await;
+        }
+    }
+
+    async fn handle_connection_closed(
+        connection_result: ConnectionError,
+        game: &Arc<Mutex<Game>>,
+        player: &Option<Arc<ArcSwap<Player>>>,
+        id: u64,
+    ) -> Result<ConnectionError> {
+        println!("Connection from client {id} closed");
+
+        if let Some(player) = player {
+            println!(
+                "Despawning player {:?} from client {id}",
+                player.load().entity
+            );
+            let mut game = game.lock().await;
+            println!("Game lock acquired");
+            game.despawn_hero(player.load().entity).await;
+        }
+
+        return Ok(connection_result);
+    }
+
+    async fn handle_leaderboard_update(
+        update: LeaderboardUpdatePacket,
+        connection: &Connection,
+    ) -> Result<()> {
+        let mut update_stream = connection.open_uni().await?.await?;
+
+        update_stream.write_all(&update.to_bytes()).await?;
+        update_stream.finish().await?;
+
+        Ok(())
+    }
+
+    async fn handle_chat_broadcast(request: ChatRequest, connection: &Connection) -> Result<()> {
+        let mut update_stream = connection.open_uni().await?.await?;
+
+        update_stream.write_all(&request.to_bytes()).await?;
+        update_stream.finish().await?;
+
+        Ok(())
     }
 }
