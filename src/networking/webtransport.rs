65286634f7c1a5e04b5308e0f10f4e5547a9b1d4
elOx <elox55555@gmail.com>
2025-01-13 15:31:01 +0200

    Move webtransport handler functions out of struct impl

--- src/networking/webtransport.rs
+++ src/networking/webtransport.rs
@@ -113,202 +113,202 @@ impl WebTransportServer {
             tokio::select! {
                 stream = connection.accept_uni() => {
                     let stream = stream?;
-                    Self::handle_uni_stream(stream, &mut buffer, &connection, &game, id, &chat_tx).await?;
+                    handle_uni_stream(stream, &mut buffer, &connection, &game, id, &chat_tx).await?;
                 }
                 streams = connection.accept_bi() => {
                     let streams = streams?;
                     let (send_stream, recv_stream) = streams;
-                    Self::handle_bi_stream(send_stream, recv_stream, &mut buffer).await?;
+                    handle_bi_stream(send_stream, recv_stream, &mut buffer).await?;
                 }
                 dgram = connection.receive_datagram() => {
                     let dgram = dgram?;
-                    Self::handle_datagram(dgram, &game, id).await;
+                    handle_datagram(dgram, &game, id).await;
                 }
                 connection_result = connection.closed() => {
-                    return Self::handle_connection_closed(connection_result, &game, id).await;
+                    return handle_connection_closed(connection_result, &game, id).await;
                 }
                 leaderboard_update = lb_rx.recv() => {
                     let leaderboard_update = leaderboard_update?;
-                    Self::handle_leaderboard_update(leaderboard_update, &connection).await?;
+                    handle_leaderboard_update(leaderboard_update, &connection).await?;
                 }
                 chat_broadcast = chat_rx.recv() => {
                     let chat_broadcast = chat_broadcast?;
-                    Self::handle_chat_broadcast(chat_broadcast, &connection, id).await?;
+                    handle_chat_broadcast(chat_broadcast, &connection, id).await?;
                 }
             }
         }
     }
+}
 
-    async fn handle_uni_stream(
-        mut stream: RecvStream,
-        buffer: &mut Box<[u8]>,
-        connection: &Connection,
-        game: &Arc<Mutex<Game>>,
-        id: u64,
-        chat_tx: &broadcast::Sender<ChatRequest>,
-    ) -> Result<()> {
-        let bytes_read = match stream.read(buffer).await? {
-            Some(bytes_read) => bytes_read,
-            None => return Ok(()),
-        };
+async fn handle_uni_stream(
+    mut stream: RecvStream,
+    buffer: &mut Box<[u8]>,
+    connection: &Connection,
+    game: &Arc<Mutex<Game>>,
+    id: u64,
+    chat_tx: &broadcast::Sender<ChatRequest>,
+) -> Result<()> {
+    let bytes_read = match stream.read(buffer).await? {
+        Some(bytes_read) => bytes_read,
+        None => return Ok(()),
+    };
 
-        let data = &buffer[..bytes_read];
-        let header = &data[0..4];
-        let data = &data[4..];
+    let data = &buffer[..bytes_read];
+    let header = &data[0..4];
+    let data = &data[4..];
 
-        match header {
-            b"NAME" => {
-                let name = std::str::from_utf8(data)?;
+    match header {
+        b"NAME" => {
+            let name = std::str::from_utf8(data)?;
 
-                println!("Accepted name '{name}' from client {id}. Spawning hero...");
+            println!("Accepted name '{name}' from client {id}. Spawning hero...");
 
-                let mut game = game.lock().await;
-                let leaderboard_state = game.leaderboard_state.clone();
+            let mut game = game.lock().await;
+            let leaderboard_state = game.leaderboard_state.clone();
 
-                let player = game.spawn_hero(id, name, connection.clone()).await;
+            let player = game.spawn_hero(id, name, connection.clone()).await;
 
-                let area = player.load().area.clone();
+            let area = player.load().area.clone();
 
-                let definition = area.lock().await.definition_packet();
+            let definition = area.lock().await.definition_packet();
 
-                if !leaderboard_state.is_empty() {
-                    let mut state_stream = connection.open_uni().await?.await?;
-                    state_stream
-                        .write_all(&leaderboard_state.to_bytes())
-                        .await?;
-                    state_stream.finish().await?;
-                }
-
-                let mut def_stream = connection.open_uni().await?.await?;
-                def_stream.write_all(&definition).await?;
-                def_stream.finish().await?;
+            if !leaderboard_state.is_empty() {
+                let mut state_stream = connection.open_uni().await?.await?;
+                state_stream
+                    .write_all(&leaderboard_state.to_bytes())
+                    .await?;
+                state_stream.finish().await?;
             }
-            b"CHAT" => {
-                let text = std::str::from_utf8(data)?;
 
-                if text.starts_with("/") {
-                    let text = &text[1..];
-                    let splits = text.split(" ").collect::<Vec<&str>>();
-                    let command = splits[0];
-                    let args = splits[1..].iter().map(|s| s.to_string()).collect();
+            let mut def_stream = connection.open_uni().await?.await?;
+            def_stream.write_all(&definition).await?;
+            def_stream.finish().await?;
+        }
+        b"CHAT" => {
+            let text = std::str::from_utf8(data)?;
 
-                    let req = CommandRequest {
-                        args,
-                        game: game.clone(),
-                        player_id: id,
+            if text.starts_with("/") {
+                let text = &text[1..];
+                let splits = text.split(" ").collect::<Vec<&str>>();
+                let command = splits[0];
+                let args = splits[1..].iter().map(|s| s.to_string()).collect();
+
+                let req = CommandRequest {
+                    args,
+                    game: game.clone(),
+                    player_id: id,
+                };
+
+                let response = handle_command(command, req).await;
+
+                let message = match response {
+                        Ok(response) => response,
+                        Err(err) => Some(ChatRequest::new(
+                            format!("A server error has occurred. Please report it to the developers: *{err:?}*"),
+                            String::new(),
+                            ChatMessageType::ServerError,
+                            Some(vec![id]),
+                        )),
                     };
 
-                    let response = handle_command(command, req).await;
-
-                    let message = match response {
-                            Ok(response) => response,
-                            Err(err) => Some(ChatRequest::new(
-                                format!("A server error has occurred. Please report it to the developers: *{err:?}*"),
-                                String::new(),
-                                ChatMessageType::ServerError,
-                                Some(vec![id]),
-                            )),
-                        };
-
-                    if let Some(message) = message {
-                        let _ = chat_tx.send(message);
-                    }
-                } else {
-                    let game = game.lock().await;
-                    let name = game.get_player(id)?.name.clone();
-
-                    let request =
-                        ChatRequest::new(text.to_owned(), name, ChatMessageType::Normal, None);
-
-                    let _ = chat_tx.send(request);
+                if let Some(message) = message {
+                    let _ = chat_tx.send(message);
                 }
-            }
-            _ => {
-                println!(
-                    "Received unknown packet from client {id} (header: {})",
-                    std::str::from_utf8(header).unwrap_or(&format!("{header:x?}").clone())
-                );
-            }
-        }
+            } else {
+                let game = game.lock().await;
+                let name = game.get_player(id)?.name.clone();
 
-        Ok(())
-    }
+                let request =
+                    ChatRequest::new(text.to_owned(), name, ChatMessageType::Normal, None);
 
-    async fn handle_bi_stream(
-        mut send_stream: SendStream,
-        mut recv_stream: RecvStream,
-        buffer: &mut Box<[u8]>,
-    ) -> Result<()> {
-        let bytes_read = match recv_stream.read(buffer).await? {
-            Some(bytes_read) => bytes_read,
-            None => return Ok(()),
-        };
-
-        let data = &buffer[..bytes_read];
-        let text = std::str::from_utf8(data)?;
-
-        if text == "ping" {
-            send_stream.write_all(b"pong").await?;
-        }
-
-        send_stream.finish().await?;
-
-        Ok(())
-    }
-
-    async fn handle_datagram(datagram: Datagram, game: &Arc<Mutex<Game>>, id: u64) {
-        let payload = datagram.payload();
-
-        let x = f32::from_le_bytes([payload[0], payload[1], payload[2], payload[3]]);
-        let y = f32::from_le_bytes([payload[4], payload[5], payload[6], payload[7]]);
-
-        // println!("Received input '({x:.2}, {y:.2})' from client {id}");
-
-        let mut game = game.lock().await;
-        let _ = game.update_player_input(id, Vec2::new(x, y)).await;
-    }
-
-    async fn handle_connection_closed(
-        connection_result: ConnectionError,
-        game: &Arc<Mutex<Game>>,
-        id: u64,
-    ) -> Result<ConnectionError> {
-        println!("Connection from client {id} closed");
-
-        let mut game = game.lock().await;
-        let _ = game.despawn_hero(id).await;
-
-        Ok(connection_result)
-    }
-
-    async fn handle_leaderboard_update(
-        update: LeaderboardUpdate,
-        connection: &Connection,
-    ) -> Result<()> {
-        let mut update_stream = connection.open_uni().await?.await?;
-
-        update_stream.write_all(&update.to_bytes()).await?;
-        update_stream.finish().await?;
-
-        Ok(())
-    }
-
-    async fn handle_chat_broadcast(
-        request: ChatRequest,
-        connection: &Connection,
-        id: u64,
-    ) -> Result<()> {
-        if let Some(ref filter) = request.recipient_filter {
-            if !filter.contains(&id) {
-                return Ok(());
+                let _ = chat_tx.send(request);
             }
         }
-
-        let mut update_stream = connection.open_uni().await?.await?;
-
-        update_stream.write_all(&request.to_bytes()).await?;
-        update_stream.finish().await?;
-
-        Ok(())
+        _ => {
+            println!(
+                "Received unknown packet from client {id} (header: {})",
+                std::str::from_utf8(header).unwrap_or(&format!("{header:x?}").clone())
+            );
+        }
     }
+
+    Ok(())
+}
+
+async fn handle_bi_stream(
+    mut send_stream: SendStream,
+    mut recv_stream: RecvStream,
+    buffer: &mut Box<[u8]>,
+) -> Result<()> {
+    let bytes_read = match recv_stream.read(buffer).await? {
+        Some(bytes_read) => bytes_read,
+        None => return Ok(()),
+    };
+
+    let data = &buffer[..bytes_read];
+    let text = std::str::from_utf8(data)?;
+
+    if text == "ping" {
+        send_stream.write_all(b"pong").await?;
+    }
+
+    send_stream.finish().await?;
+
+    Ok(())
+}
+
+async fn handle_datagram(datagram: Datagram, game: &Arc<Mutex<Game>>, id: u64) {
+    let payload = datagram.payload();
+
+    let x = f32::from_le_bytes([payload[0], payload[1], payload[2], payload[3]]);
+    let y = f32::from_le_bytes([payload[4], payload[5], payload[6], payload[7]]);
+
+    // println!("Received input '({x:.2}, {y:.2})' from client {id}");
+
+    let mut game = game.lock().await;
+    let _ = game.update_player_input(id, Vec2::new(x, y)).await;
+}
+
+async fn handle_connection_closed(
+    connection_result: ConnectionError,
+    game: &Arc<Mutex<Game>>,
+    id: u64,
+) -> Result<ConnectionError> {
+    println!("Connection from client {id} closed");
+
+    let mut game = game.lock().await;
+    let _ = game.despawn_hero(id).await;
+
+    Ok(connection_result)
+}
+
+async fn handle_leaderboard_update(
+    update: LeaderboardUpdate,
+    connection: &Connection,
+) -> Result<()> {
+    let mut update_stream = connection.open_uni().await?.await?;
+
+    update_stream.write_all(&update.to_bytes()).await?;
+    update_stream.finish().await?;
+
+    Ok(())
+}
+
+async fn handle_chat_broadcast(
+    request: ChatRequest,
+    connection: &Connection,
+    id: u64,
+) -> Result<()> {
+    if let Some(ref filter) = request.recipient_filter {
+        if !filter.contains(&id) {
+            return Ok(());
+        }
+    }
+
+    let mut update_stream = connection.open_uni().await?.await?;
+
+    update_stream.write_all(&request.to_bytes()).await?;
+    update_stream.finish().await?;
+
+    Ok(())
 }
